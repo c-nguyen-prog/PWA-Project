3ef080bff0e79803eb85444c92581a9a7ccfa8f4
Santiago Escobar Martinez Acevedo Betancur <sanesma93@gmail.com>
2018-12-11 17:53:47 +0100

    execution mode data binding

--- ionicApp/src/pages/new-transaction/new-transaction.ts
+++ ionicApp/src/pages/new-transaction/new-transaction.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import {Transaction} from "../../app/interfaces/iTransaction";
+import {setExistingDeepLinkConfig} from "@ionic/app-scripts/dist/deep-linking";
 /**
  * Generated class for the NewTransactionPage page.
  *
@@ -14,8 +15,21 @@ import {Transaction} from "../../app/interfaces/iTransaction";
   templateUrl: 'new-transaction.html',
 })
 export class NewTransactionPage {
-  public transaction: Transaction;
+  execMode: string;
+  private execNow: boolean = true;
+
+  public setExecNow(value: boolean) {
+    this.execNow = value;
+  }
+
+  public getExecNow(): boolean{
+    return this.getExecNow();
+  }
+
+    public transaction: Transaction;
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+    this.setExecNow(true);
+    this.execMode = "now";
   }
 
   ionViewDidLoad() {
