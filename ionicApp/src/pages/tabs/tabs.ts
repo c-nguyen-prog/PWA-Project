8ead9655672fc5a92037f662ea1c95dd4053b7bc
Simon Zibat <40355788+PhysCeres@users.noreply.github.com>
2018-12-16 10:23:26 +0100

    updates
    added blue toolbar with white font to search, listview and settings pages
    added split pane to tabspage. Currently broken so disabled, but i need it on laptop

--- ionicApp/src/pages/tabs/tabs.ts
+++ ionicApp/src/pages/tabs/tabs.ts
@@ -1,14 +1,21 @@
-import { Component } from '@angular/core';
+import {Component, ViewChild} from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
-import { IonicPage, NavController } from 'ionic-angular';
+import { IonicPage, Nav, NavController, MenuController} from 'ionic-angular';
 
 import { Tab1Root, Tab2Root, Tab3Root } from '../';
+
+interface PageItem {
+  title: string
+  component: any
+}
+type PageList = PageItem[]
 
 @IonicPage()
 @Component({
   selector: 'page-tabs',
-  templateUrl: 'tabs.html'
+  templateUrl: 'tabs.html',
 })
+
 export class TabsPage {
   tab1Root: any = Tab1Root;
   tab2Root: any = Tab2Root;
@@ -18,11 +25,29 @@ export class TabsPage {
   tab2Title = " ";
   tab3Title = " ";
 
+  @ViewChild(Nav) nav: Nav;
+
+  rootPage= 'ListMasterPage';
+
+  overviewPages: PageList;
+
   constructor(public navCtrl: NavController, public translateService: TranslateService) {
     translateService.get(['TAB1_TITLE', 'TAB2_TITLE', 'TAB3_TITLE']).subscribe(values => {
       this.tab1Title = values['TAB1_TITLE'];
       this.tab2Title = values['TAB2_TITLE'];
       this.tab3Title = values['TAB3_TITLE'];
     });
+
+    this.overviewPages = [
+      { title: 'Overview', component: 'ListMasterPage' },
+      { title: 'Search', component: 'SearchPage' },
+      { title: 'Personal Data', component: 'SettingsPage'}
+    ];
+  }
+
+  openPage(page: PageItem) {
+    // Reset the content nav to have just this page
+    // we wouldn't want the back button to show in this scenario
+    this.nav.setRoot(page.component);
   }
 }
