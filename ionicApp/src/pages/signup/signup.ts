4bdaac5725fde085aa32057e1a6ac1477c6ce7f3
PhysCeres <simon.zibat@gmail.com>
2018-12-17 10:53:42 +0100

    signup validation

--- ionicApp/src/pages/signup/signup.ts
+++ ionicApp/src/pages/signup/signup.ts
@@ -3,9 +3,10 @@ import { TranslateService } from '@ngx-translate/core';
 import { IonicPage, NavController, ToastController } from 'ionic-angular';
 import { User } from '../../providers';
 import { MainPage } from '../';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
 
 import Hashes from "jshashes";
+import {V} from "@angular/core/src/render3";
 
 @IonicPage()
 @Component({
@@ -47,19 +48,48 @@ export class SignupPage{
 
   // Our translated text strings
   private signupErrorString: string;
+  submitForm: FormGroup;
 
   constructor(public navCtrl: NavController,
               public user: User,
               public toastCtrl: ToastController,
-              public translateService: TranslateService) {
+              public translateService: TranslateService,
+              public formBuilder: FormBuilder) {
 
     this.translateService.get('SIGNUP_ERROR').subscribe((value) => {
       this.signupErrorString = value;
-    })
+    });
+
+    this.submitForm= formBuilder.group({
+      title: ['', Validators.required],
+      firstName: ['', Validators.compose([Validators.required, Validators.pattern('[a-zA-Z]*')])],
+      lastName: ['', Validators.compose([Validators.required, Validators.pattern('[a-zA-Z]*')])],
+      birthdate: ['', Validators.required],
+      email: ['', Validators.compose([Validators.required, Validators.pattern('^[a-zA-Z0-9._]+[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$')])], //https://forum.ionicframework.com/t/email-regex/102423/6
+      password: ['', Validators.compose([Validators.required, Validators.pattern('^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$')])], //https://stackoverflow.com/questions/19605150/regex-for-password-must-contain-at-least-eight-characters-at-least-one-number-a
+      passwordValidation: ['', Validators.compose([Validators.required])],
+      phone: ['', Validators.compose([Validators.required, Validators.maxLength(11), Validators.pattern('[0-9]*')])],
+      address: ['', Validators.required],
+      zipcode: ['', Validators.compose([Validators.required, Validators.pattern('[0-9]{5}')])],
+      city: ['', Validators.compose([Validators.required, Validators.pattern('[a-zA-Z]*')])],
+      tin: ['', Validators.compose([Validators.required, Validators.pattern('[0-9]{11}')])],
+      nationality: ['', Validators.compose([Validators.required, Validators.pattern('[a-zA-Z]*')])],
+    }, {validator: SignupPage.match});
   }
 
+  //https://forum.ionicframework.com/t/i-need-help-to-confirm-email-and-password/73668/2
+  static match(cg: FormGroup): {[err: string]: any} {
+    let pwd1 = cg.get('password');
+    let pwd2 = cg.get('passwordValidation');
+    let rv: {[error: string]: any} = {};
+    if ((pwd1.touched || pwd2.touched) && pwd1.value !== pwd2.value) {
+      rv['passwordMismatch'] = true;
+    }
+    return rv;
+  }
 
   doSignup() {
+
     // Attempt to login in through our User service
     let tempAccount = JSON.parse(JSON.stringify(this.account));
     tempAccount.password = new Hashes.SHA512().hex(tempAccount.password);
