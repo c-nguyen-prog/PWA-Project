9785c60f19a986dc4c54685448ccf80dc1642cd3
Simon Zibat <40355788+PhysCeres@users.noreply.github.com>
2018-12-30 22:00:21 +0100

    overview prototype
    json to list implemented, uses placeholders atm

--- ionicApp/src/pages/list-master/list-master.ts
+++ ionicApp/src/pages/list-master/list-master.ts
@@ -3,6 +3,8 @@ import { IonicPage, ModalController, NavController } from 'ionic-angular';
 
 import { Item } from '../../models/item';
 import { Items } from '../../providers';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
 
 @IonicPage()
 @Component({
@@ -10,45 +12,20 @@ import { Items } from '../../providers';
   templateUrl: 'list-master.html'
 })
 export class ListMasterPage {
-  currentItems: Item[];
-
-  constructor(public navCtrl: NavController, public items: Items, public modalCtrl: ModalController) {
-    this.currentItems = this.items.query();
+  public transactions:any;
+  constructor(public navCtrl: NavController, public http: HttpClient) {
+    this.loadTransactions();
   }
 
-  /**
-   * The view loaded, let's query our items for the list
-   */
-  ionViewDidLoad() {
-  }
-
-  /**
-   * Prompt the user to add a new item. This shows our ItemCreatePage in a
-   * modal and then adds the new item to our data source if the user created one.
-   */
-  addItem() {
-    let addModal = this.modalCtrl.create('ItemCreatePage');
-    addModal.onDidDismiss(item => {
-      if (item) {
-        this.items.add(item);
-      }
+  loadTransactions(){
+    let data:Observable<any>;
+    data = this.http.get('https://jsonplaceholder.typicode.com/posts');
+    data.subscribe(result=>{
+      this.transactions = result;
     })
-    addModal.present();
   }
 
-  /**
-   * Delete an item from the list of items.
-   */
-  deleteItem(item) {
-    this.items.delete(item);
-  }
-
-  /**
-   * Navigate to the detail page for this item.
-   */
-  openItem(item: Item) {
-    this.navCtrl.push('ItemDetailPage', {
-      item: item
-    });
+  transactionClick(transaction:number){
+    alert(transaction);
   }
 }
