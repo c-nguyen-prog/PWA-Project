2a7ef0946b61dcdca1b88240684efa2945b21a98
Santiago Escobar Martinez Acevedo Betancur <sanesma93@gmail.com>
2019-01-26 17:39:00 +0100

    added functions for push notifications to new transaction, typescript version error persists, need to upgrade to TSv3.0+

--- ionicApp/src/app/sw.js
+++ ionicApp/src/app/sw.js
@@ -19,30 +19,30 @@
 
 /* eslint-env browser, serviceworker, es6 */
 
-'use strict';
-self.addEventListener('push', function(event) {
-  console.log('[Service Worker] Push Received.');
-  console.log(`[Service Worker] Push had this data: "${event.data.text()}"`);
+  'use strict';
+  self.addEventListener('push', function(event) {
+    console.log('[Service Worker] Push Received.');
+    console.log(`[Service Worker] Push had this data: "${event.data.text()}"`);
 
-  const title = 'Jeb Bush sends his regards';
-  const options = {
-    body: 'Please clap.',
-    icon: 'images/icon.png',
-    badge: 'images/badge.png'
-  };
+    const title = 'Jeb Bush sends his regards';
+    const options = {
+      body: 'Please clap.',
+      icon: 'images/icon.png',
+      badge: 'images/badge.png'
+    };
 
-  const notificationPromise = self.registration.showNotification(title, options);
-event.waitUntil(notificationPromise);
+    const notificationPromise = self.registration.showNotification(title, options);
+  event.waitUntil(notificationPromise);
 
-});
+  });
 
 
-self.addEventListener('notificationclick', function(event) {
-  console.log('[Service Worker] Notification click Received.');
+  self.addEventListener('notificationclick', function(event) {
+    console.log('[Service Worker] Notification click Received.');
 
-  event.notification.close();
+    event.notification.close();
 
-  event.waitUntil(
-    clients.openWindow('https://jeb2016.com/')
-  );
-});
+    event.waitUntil(
+      clients.openWindow('https://jeb2016.com/')
+    );
+  });
--- ionicApp/src/pages/new-transaction/new-transaction.ts
+++ ionicApp/src/pages/new-transaction/new-transaction.ts
@@ -23,6 +23,12 @@ import {equals} from "@ngx-translate/core/src/util";
   templateUrl: 'new-transaction.html',
 })
 export class NewTransactionPage {
+  readonly applicationServerPublicKey = 'BDFc2s7Haf2s9lt9ttYZGvwV366dP78zP-xps4Z3sKx9k_u3Wbb56vzC4FXMZJPyGZx_X7ke6rtKk1dCWok68N4';
+  readonly pushButton = document.querySelector('.js-push-btn');
+
+  isSubscribed = false;
+  swRegistration = null;
+
   submitAttempt: boolean = false;
   transactionForm: FormGroup;
   templates: Template[];
@@ -70,6 +76,31 @@ export class NewTransactionPage {
     return this.execLater;
   }
   constructor( public formBuilder: FormBuilder, private _cdr: ChangeDetectorRef,   public toastCtrl: ToastController, public navCtrl: NavController, public navParams: NavParams, public userService: User, public transferServicerino: TransferService, public templateServicerino: TemplateService) {
+
+    if ('serviceWorker' in navigator && 'PushManager' in window) {
+      console.log('Service Worker and Push is supported');
+
+      navigator.serviceWorker.register('sw.js')
+        .then(function(swReg) {
+          console.log('Service Worker is registered', swReg);
+
+          this.swRegistration = swReg;
+          initializeUI();
+
+        })
+        .catch(function(error) {
+          console.error('Service Worker Error', error);
+        });
+    } else {
+      console.warn('Push messaging is not supported');
+      pushButton.textContent = 'Push Not Supported';
+    }
+
+
+
+
+
+
 
     this.setExecLater(false);
     this.transaction.type = "now";
@@ -195,6 +226,131 @@ export class NewTransactionPage {
     });
   }
 
+  urlB64ToUint8Array(base64String) {
+    const padding = '='.repeat((4 - base64String.length % 4) % 4);
+    const base64 = (base64String + padding)
+      .replace(/\-/g, '+')
+      .replace(/_/g, '/');
+
+    const rawData = window.atob(base64);
+    const outputArray = new Uint8Array(rawData.length);
+
+    for (let i = 0; i < rawData.length; ++i) {
+      outputArray[i] = rawData.charCodeAt(i);
+    }
+    return outputArray;
+  }
+
+
+
+   updateBtn() {
+    if (Notification.permission === 'denied') {
+      this.pushButton.textContent = 'Push Messaging Blocked.';
+      this.pushButton.disabled = true;
+      this.updateSubscriptionOnServer(null);
+      return;
+    }
+
+    if (this.isSubscribed) {
+      this.pushButton.textContent = 'Disable Push Messaging';
+    } else {
+      this.pushButton.textContent = 'Enable Push Messaging';
+    }
+
+    this.pushButton.disabled = false;
+  }
+
+
+   initializeUI() {
+
+    this.pushButton.addEventListener('click', function() {
+      this.pushButton.disabled = true;
+      if (this.isSubscribed) {
+        //  Unsubscribe user
+        this.unsubscribeUser();
+
+      } else {
+        this.subscribeUser();
+      }
+    });
+
+
+
+    // Set the initial subscription value
+    this.swRegistration.pushManager.getSubscription()
+      .then(function(subscription) {
+        this.isSubscribed = !(subscription === null);
+
+        this.updateSubscriptionOnServer(subscription);
+
+        if (this.isSubscribed) {
+          console.log('User IS subscribed.');
+        } else {
+          console.log('User is NOT subscribed.');
+        }
+
+        this.updateBtn();
+      });
+  }
+
+
+   subscribeUser() {
+    const applicationServerKey = this.urlB64ToUint8Array(this.applicationServerPublicKey);
+    this.swRegistration.pushManager.subscribe({
+      userVisibleOnly: true,
+      applicationServerKey: applicationServerKey
+    })
+      .then(function(subscription) {
+        console.log('User is subscribed.');
+
+        this.updateSubscriptionOnServer(subscription);
+
+        this.isSubscribed = true;
+
+        this.updateBtn();
+      })
+      .catch(function(err) {
+        console.log('Failed to subscribe the user: ', err);
+        this.updateBtn();
+      });
+  }
+
+
+   updateSubscriptionOnServer(subscription) {
+    // Send subscription to application server
+
+    const subscriptionJson = document.querySelector('.js-subscription-json');
+    const subscriptionDetails =
+      document.querySelector('.js-subscription-details');
+
+    if (subscription) {
+      subscriptionJson.textContent = JSON.stringify(subscription);
+      subscriptionDetails.classList.remove('is-invisible');
+    } else {
+      subscriptionDetails.classList.add('is-invisible');
+    }
+  }
+
+
+   unsubscribeUser() {
+    this.swRegistration.pushManager.getSubscription()
+      .then(function(subscription) {
+        if (subscription) {
+          return subscription.unsubscribe();
+        }
+      })
+      .catch(function(error) {
+        console.log('Error unsubscribing', error);
+      })
+      .then(function() {
+        this.updateSubscriptionOnServer(null);
+
+        console.log('User is unsubscribed.');
+        this.isSubscribed = false;
+
+        this.updateBtn();
+      });
+  }
 
 
 
